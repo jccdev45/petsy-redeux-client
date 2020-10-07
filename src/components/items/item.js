7b540fee7ca46e6d11785c66c1dabd06753bcb32
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2020-10-07 10:41:48 -0400

    Render star component from item rating

--- src/components/items/item.js
+++ src/components/items/item.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FcRating } from "react-icons/fc";
 import { Link } from "react-router-dom";
 import Carousel from "../carousel/carousel";
 import ConfirmDeleteModal from "../modal/confirmDeleteModal";
@@ -21,6 +22,17 @@ export default function Item({ item, user, deletion }) {
       );
     }
   };
+
+  const renderStars = () => {
+    let ratings = [];
+    let i = 1;
+    while (i <= item.rating) {
+      i++;
+      ratings.push(<FcRating key={i} />);
+    }
+    return <div className="flex items-center">{ratings}</div>;
+  };
+
   return (
     <article
       className="flex flex-col w-2/3 p-4 rounded-lg shadow-lg md:w-1/3 lg:w-1/4 md:mx-4"
@@ -28,8 +40,20 @@ export default function Item({ item, user, deletion }) {
     >
       <Carousel item={item} height="h-64" />
 
-      <Link to={`/items/${item.id}`}>{item.name}</Link>
-      <span>${item.price}.00</span>
+      <div className="flex items-center justify-between text-lg">
+        <Link
+          to={`/items/${item.id}`}
+          className="text-red-300 underline hover:text-red-400"
+        >
+          {item.name}
+        </Link>
+        {renderStars()}
+      </div>
+
+      <div>
+        <span className="text-green-500">$</span>
+        <span className="mx-1 bold">{item.price}.00</span>
+      </div>
 
       {user && user.id === item.user_id ? (
         <div className="flex items-center justify-evenly">
