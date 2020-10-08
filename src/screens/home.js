92723d1dde328cea3a4097c3b33517df56e34c8c
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2020-10-08 10:47:58 -0400

    Added data hook, refactored Items

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,67 +1,42 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router";
-import axios from "axios";
+import React from "react";
+import { Redirect, Route, Switch } from "react-router";
+import Items from "../screens/items/items";
+import ItemDetails from "../screens/items/itemDetails";
+import ItemCreate from "../screens/items/itemCreate";
+import Profile from "../screens/user/profile";
+import ItemEdit from "../screens/items/itemEdit";
+import { ProviderData } from "../util/hooks/useFetchData";
 
-import SecondaryRoutes from "../routes/secondaryRoutes";
-import {
-  addItem,
-  deleteItem,
-  editItem,
-  getItems,
-} from "../util/items/itemMethods";
-
-export default function Home({ user }) {
-  const [items, setItems] = useState([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
-  const history = useHistory();
-
-  useEffect(() => {
-    const cancelToken = axios.CancelToken.source();
-
-    const fetchData = async () => {
-      await getItems()
-        .then((res) => setItems(res), setIsLoading(false))
-        .catch((e) => {
-          if (axios.isCancel(e)) return;
-          setError(e);
-        });
-    };
-    fetchData();
-    return () => {
-      cancelToken.cancel();
-    };
-  }, []);
-
-  const updateItem = async (id, data) => {
-    const updated = await editItem(id, data);
-    setItems((items) =>
-      items.map((item) => (item.id === Number(id) ? updated : item))
-    );
-    history.push("/");
-  };
-
-  const addNewItem = async (data) => {
-    const newItem = await addItem(data);
-    setItems((items) => [...items, newItem]);
-    history.push("/");
-  };
-
-  const deletion = async (id) => {
-    await deleteItem(id);
-    setItems((items) => items.filter((item) => item.id !== id));
-    history.push("/");
-  };
-
+export default function Home() {
   return (
-    <SecondaryRoutes
-      user={user}
-      items={items}
-      loading={isLoading}
-      error={error}
-      update={updateItem}
-      addNew={addNewItem}
-      deletion={deletion}
-    />
+    <ProviderData>
+      <Switch>
+        <Route path="/items/new">
+          <ItemCreate />
+        </Route>
+        <Route path="/items/:id/edit">
+          <ItemEdit />
+        </Route>
+        <Route exact path="/items">
+          <Items />
+        </Route>
+        <Route exact path="/items/:id">
+          <ItemDetails />
+        </Route>
+        <Route path="/users/:id">
+          <Profile />
+        </Route>
+        <Redirect from="/" to="/items" exact />
+      </Switch>
+    </ProviderData>
+    // <SecondaryRoutes
+    //   user={user}
+    //   items={items}
+    //   loading={isLoading}
+    //   error={error}
+    //   update={updateItem}
+    //   addNew={addNewItem}
+    //   deletion={deletion}
+    // />
   );
 }
