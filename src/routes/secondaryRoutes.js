6794192138d741dac83e0e6a941bfe902bb853dd
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2020-10-10 07:39:03 -0400

    Remove comments

--- src/routes/secondaryRoutes.js
+++ src/routes/secondaryRoutes.js
@@ -13,11 +13,9 @@ const SecondaryRoutes = () => {
         <ItemCreate />
       </Route>
       <Route path="/items/:id/edit">
-        {/* DONE: refactored for new data hook */}
         <ItemEdit />
       </Route>
       <Route exact path="/items">
-        {/* DONE: refactored for new data hook */}
         <Items />
       </Route>
       <Route exact path="/items/:id">
