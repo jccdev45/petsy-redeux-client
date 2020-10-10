1e153ce18b466c2ee5fc48e6925e48e23d7e7277
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2020-10-10 05:13:39 -0400

    Changed LS to use "Petsy" prefix

--- src/util/auth/auth.js
+++ src/util/auth/auth.js
@@ -1,23 +1,26 @@
 import { api } from "../api/apiConfig";
 
+const LS_TOKEN = "petsy/authToken";
+const LS_USER = "petsy/user";
+
 export const loginUser = async (data) => {
   const res = await api.post("/auth/login", { authentication: data });
-  localStorage.setItem("authToken", res.data.token);
-  localStorage.setItem("user", JSON.stringify(res.data.user));
+  localStorage.setItem(LS_TOKEN, res.data.token);
+  localStorage.setItem(LS_USER, JSON.stringify(res.data.user));
   api.defaults.headers.common.authorization = `Bearer ${res.data.token}`;
   return res.data.user;
 };
 
 export const registerUser = async (data) => {
   const res = await api.post("/users", { user: data });
-  localStorage.setItem("authToken", res.data.token);
-  localStorage.setItem("user", JSON.stringify(res.data.user));
+  localStorage.setItem(LS_TOKEN, res.data.token);
+  localStorage.setItem(LS_USER, JSON.stringify(res.data.user));
   api.defaults.headers.common.authorization = `Bearer ${res.data.token}`;
   return res.data.user;
 };
 
 export const verifyUser = async () => {
-  const token = localStorage.getItem("authToken");
+  const token = localStorage.getItem(LS_TOKEN);
   if (token) {
     api.defaults.headers.common.authorization = `Bearer ${token}`;
     const res = await api.get("/auth/verify");
@@ -28,5 +31,5 @@ export const verifyUser = async () => {
 
 export const removeToken = () => {
   api.defaults.headers.common.authorization = null;
-  localStorage.removeItem("user")
+  localStorage.removeItem(LS_USER);
 };
