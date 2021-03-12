ec61f2215fb9cbd913670cc3d9e64175d8f45381
JordanCruzCorrea <jccdev45@gmail.com>
2021-03-12 16:07:38 -0500

    Added site info section

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { FcRating } from "react-icons/fc";
+import {
+	FcRating,
+	FcShipped,
+	FcMoneyTransfer,
+	FcOnlineSupport,
+	FcGlobe,
+} from "react-icons/fc";
 import { Link } from "react-router-dom";
 import Hero from "../components/hero/hero";
 import Item from "../components/items/item";
@@ -18,7 +24,7 @@ export default function Home() {
 		return slicedItems;
 	};
 
-	const featuredItem = () => {
+	const renderFeaturedItem = () => {
 		const randomItem = items[Math.floor(Math.random() * items.length)];
 
 		const renderStars = () => {
@@ -28,7 +34,7 @@ export default function Home() {
 				i++;
 				ratings.push(<FcRating key={i} />);
 			}
-			return <div className="flex items-center">{ratings}</div>;
+			return <div className="flex items-center text-3xl">{ratings}</div>;
 		};
 
 		return (
@@ -60,12 +66,44 @@ export default function Home() {
 		<View class="flex flex-col items-center justify-center pt-24 md:w-5/6 mx-auto">
 			<Hero />
 			{isLoading && <Loader />}
+
+      {/* New Items */}
 			<View title="New Items" class="flex flex-col w-full">
 				<div className="flex w-full">{newItems()}</div>
 			</View>
+
+      {/* Featured Item */}
 			<View title="Featured Item" class="w-full flex flex-col my-8">
 				<div className="flex items-center justify-end w-full p-8 bg-red-100 rounded-lg">
-					{featuredItem()}
+					{renderFeaturedItem()}
+				</div>
+			</View>
+
+      {/* Site Info */}
+			<View class="bg-red-200 rounded-lg p-8 flex justify-evenly my-8 w-full">
+				<div className="flex flex-col items-center">
+					<div className="flex flex-col items-center justify-center w-24 h-24 bg-red-100 rounded-full">
+						<FcShipped className="text-5xl" />
+					</div>
+					<h2 className="my-4 text-2xl text-white">Free Returns</h2>
+				</div>
+				<div className="flex flex-col items-center">
+					<div className="flex flex-col items-center justify-center w-24 h-24 bg-red-100 rounded-full">
+						<FcMoneyTransfer className="text-5xl" />
+					</div>
+					<h2 className="my-4 text-2xl text-white">Payment Plans</h2>
+				</div>
+				<div className="flex flex-col items-center">
+					<div className="flex flex-col items-center justify-center w-24 h-24 bg-red-100 rounded-full">
+						<FcOnlineSupport className="text-5xl" />
+					</div>
+					<h2 className="my-4 text-2xl text-white">24/7 Support</h2>
+				</div>
+				<div className="flex flex-col items-center">
+					<div className="flex flex-col items-center justify-center w-24 h-24 bg-red-100 rounded-full">
+						<FcGlobe className="text-5xl" />
+					</div>
+					<h2 className="my-4 text-2xl text-white">Worldwide Shipping</h2>
 				</div>
 			</View>
 			{error && <h1>do a refresh</h1>}
